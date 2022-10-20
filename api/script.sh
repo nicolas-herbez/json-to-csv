b7b099a9d8110245e7447bf4c34dedf81fca2b74
Nicolas Herbez <nicolas.herbez@outlook.fr>
2022-10-20 10:10:16 +0200

    install PHPUnit

--- api/script.sh
+++ api/script.sh
@@ -7,6 +7,8 @@ clear
 echo "\n==> You wish :" \
     "\n$color [1]$nc Run PHP_CodeSniffer" \
     "\n$color [2]$nc Run PHPStan" \
+    "\n$color [3]$nc Run tests" \
+    "\n$color [4]$nc Run coverage test" \
     "\n$color [ ]$nc Use the docker terminal"
 
 read -p "Choose a number [ ] : " response
@@ -17,6 +19,11 @@ if [ -n $0 ] && [ "$response" = 1 ]; then
     ./vendor/bin/phpcs -v --standard=PSR12 --ignore=./src/Kernel.php ./src
 elif [ -n $0 ] && [ "$response" = 2 ]; then
     ./vendor/bin/phpstan analyse ./src
+elif [ -n $0 ] && [ "$response" = 3 ]; then
+    php bin/phpunit --testdox
+elif [ -n $0 ] && [ "$response" = 4 ]; then
+    php bin/phpunit --coverage-html var/log/test/test-coverage
+    echo "\n==> You can see the result with$color /api/var/log/test/test-coverage/index.html$nc"
 else
     bash
 fi
