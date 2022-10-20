b25ae9865528def2b8998adbeadf47b93b7da013
Nicolas Herbez <nicolas.herbez@outlook.fr>
2022-10-20 14:20:05 +0200

    add Power entity tests

---
+++ api/tests/Entity/PowerTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Tests\Entity;
+
+use App\Entity\Power;
+use PHPUnit\Framework\TestCase;
+
+class PowerTest extends TestCase
+{
+    private Power $power;
+    
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->power = new Power();
+    }
+
+    public function testGetId(): void
+    {
+        $this->assertTrue($this->power->getId() === null);
+    }
+
+    public function testSetAndGetName(): void
+    {
+        $name = "Test power";
+        $response = $this->power->setName($name);
+
+        $this->assertInstanceOf(Power::class, $response);
+        $this->assertEquals($name, $this->power->getName());
+    }
+
+    public function testSetAndGetCode(): void
+    {
+        $code = "TP";
+        $response = $this->power->setCode($code);
+
+        $this->assertInstanceOf(Power::class, $response);
+        $this->assertEquals($code, $this->power->getCode());
+    }
+}
